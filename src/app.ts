854d136240a8b2e1c79bbd2c5e43ce78abb8e311
Zillur2023 <zillurrahmanbd12@gmail.com>
2024-09-06 03:21:56 +0600

    orinin update

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(cors({ origin: ['https://assignment-four-client.vercel.app'], credentials: true }));
-// app.use(cors({ origin: [config.client_url as string], credentials: true }));
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: [config.client_url as string], credentials: true }));
 
 // application routes
 app.use("/api/v1", router);
